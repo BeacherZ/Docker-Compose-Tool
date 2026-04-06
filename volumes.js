38e084031ac2e58e1c13c9bab838107023ccbc3e
BeacherZ <60752063+BeacherZ@users.noreply.github.com>
2026-04-06 15:01:34 +0800

    Update volumes.js

--- volumes.js
+++ volumes.js
@@ -1,7 +1,7 @@
 // 此文件为 index.html 的配置文件，需与 index.html 放在同一目录
 // 修改下方[卷映射绝对路径]后刷新网页即可生效
 // {container_name} 会自动替换为容器名
-VOLUME_PREFIX='/opt/docker/compose/{container_name}'
+VOLUME_PREFIX='/opt/docker/{container_name}'
 
 
 
